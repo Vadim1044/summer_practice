0832950aec539c064912c44d3b01576de0b231a6
Vadim1044 <vadimvase@gmail.com>
2025-06-26 13:38:52 +0600

    add files

---
+++ task02/Class1.cs
@@ -0,0 +1,50 @@
+﻿namespace task02;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+public class Student
+{
+    public string Name { get; set; }
+    public string Faculty { get; set; }
+    public List<int> Grades { get; set; }
+}
+public class StudentService
+{
+    private readonly List<Student> _students;
+
+    public StudentService(List<Student> students) => _students = students;
+
+    public IEnumerable<Student> GetStudentsByFaculty(string faculty)
+    {
+        return _students.Where(student => student.Faculty.Equals(faculty));
+    }
+
+    public IEnumerable<Student> GetStudentsWithMinAverageGrade(double minAverageGrade)
+    {
+        return _students.Where(student => student.Grades.Average() >= minAverageGrade);
+    }
+
+    public IEnumerable<Student> GetStudentsOrderedByName()
+    {
+        return _students.OrderBy(student => student.Name);
+    }
+
+    public ILookup<string, Student> GroupStudentsByFaculty()
+    {
+        return _students.ToLookup(student => student.Faculty);
+    }
+    public string GetFacultyWithHighestAverageGrade()
+    {
+        return _students
+            .Where(student => student.Grades.Any())
+            .GroupBy(student => student.Faculty)
+            .Select(group => new
+            {
+                Faculty = group.Key,
+                AverageGrade = group.Average(student => student.Grades.Average())
+            })
+            .OrderByDescending(faculty => faculty.AverageGrade)
+            .FirstOrDefault()?.Faculty;
+    }
+};
